9557529ebf149290c50871e1810d284fec1e7b1c
woodart8 <kibeom1145@gmail.com>
2024-10-11 10:50:56 +0900

    Feat: 요리 레시피 페이지별 조회 구현

---
+++ server/src/main/java/com/avengers/yoribogo/recipe/controller/RecipeController.java
@@ -0,0 +1,33 @@
+package com.avengers.yoribogo.recipe.controller;
+
+import com.avengers.yoribogo.common.ResponseDTO;
+import com.avengers.yoribogo.recipe.domain.Recipe;
+import com.avengers.yoribogo.recipe.dto.RecipeDTO;
+import com.avengers.yoribogo.recipe.service.RecipeServiceImpl;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/recipes")
+public class RecipeController {
+
+    private final ModelMapper modelMapper;
+    private final RecipeServiceImpl recipeService;
+
+    @Autowired
+    public RecipeController(ModelMapper modelMapper, RecipeServiceImpl recipeService) {
+        this.modelMapper = modelMapper;
+        this.recipeService = recipeService;
+    }
+
+    // 페이지 번호로 요리 레시피 목록 조회
+    @GetMapping
+    public ResponseDTO<Page<Recipe>> getRecipeByPageNo(@RequestParam Integer pageNo) {
+        Page<Recipe> recipes = recipeService.findRecipeByPageNo(pageNo);
+        return ResponseDTO.ok(recipes);
+    }
+
+}
--- server/src/main/java/com/avengers/yoribogo/recipe/domain/Recipe.java
+++ server/src/main/java/com/avengers/yoribogo/recipe/domain/Recipe.java
@@ -1,4 +1,32 @@
 package com.avengers.yoribogo.recipe.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "RECIPE")
+@Data
 public class Recipe {
+
+    @Id
+    @Column(name = "RECIPE_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long recipeId;
+
+    @Column(name = "MENU_NAME")
+    private String menuName;
+
+    @Column(name = "MENU_INGREDIENT")
+    private String menuIngredient;
+
+    @Column(name = "MENU_IMAGE")
+    private String menuImage;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "MENU_TYPE")
+    private MenuType menuType;
+
+    @Column(name = "USER_ID")
+    private Long userId;
+
 }
