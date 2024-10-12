3323b0ced4aee42d58d9f386bea78e96b2ec6544
iwillbfine <iwillbfine@naver.com>
2024-10-12 23:49:53 +0900

    inquiry 업로드

---
+++ server/src/main/java/com/avengers/yoribogo/inquiry/domain/Inquiry.java
@@ -0,0 +1,54 @@
+package com.avengers.yoribogo.inquiry.domain;
+
+import com.avengers.yoribogo.answer.domain.Answer;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "inquiry")
+public class Inquiry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int inquiryId;
+
+    @Column(name = "inquiry_title")
+    private String inquiryTitle;
+
+    @Column(name = "inquiry_content")
+    private String inquiryContent;
+
+    @Column(name = "inquiry_status")
+    private String inquiryStatus;
+
+    @Column(name = "inquiry_created_at")
+    private LocalDateTime inquiryCreatedAt;
+
+    @Column(name = "answers")
+    private Integer answers;
+
+    @Column(name = "user_id")
+    private int userId;
+
+    @OneToMany(mappedBy = "inquiryId")
+    private List<Answer> answer;
+
+    public Inquiry(String inquiryTitle, String inquiryContent, int userId) {
+        this.inquiryTitle = inquiryTitle;
+        this.inquiryContent = inquiryContent;
+        this.userId = userId;
+    }
+
+    public Inquiry(String inquiryTitle, String inquiryContent, Integer answers, int userId) {
+        this.inquiryTitle = inquiryTitle;
+        this.inquiryContent = inquiryContent;
+        this.answers = answers;
+        this.userId = userId;
+    }
+}
