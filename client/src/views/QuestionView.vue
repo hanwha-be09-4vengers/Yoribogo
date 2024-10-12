64663521b63b9087a80c97026b1179b525b1b09a
Kibeom Jeon <kibeom1145@gmail.com>
2024-10-12 23:33:06 +0900

    Feat: 질문에 대한 답 로컬스토리지에 저장

--- client/src/views/QuestionView.vue
+++ client/src/views/QuestionView.vue
@@ -8,19 +8,23 @@
         <div class="question-info">
           <span>{{ questions[qid - 1].label }}</span>
         </div>
-        <div class="card-board-container">
+        <div class="card-board-container" v-if="qid<5">
           <CardBoard
             :img="questions[qid - 1].leftImg"
             :text="questions[qid - 1].leftText"
-            @click="goNext(qid + 1)"
+            @click="goNext(questions[qid - 1].leftText, qid + 1)"
           >
           </CardBoard>
           <CardBoard
             :img="questions[qid - 1].rightImg"
             :text="questions[qid - 1].rightText"
-            @click="goNext(qid + 1)"
+            @click="goNext(questions[qid - 1].rightText, qid + 1)"
           >
           </CardBoard>
+        </div>
+        <div class="input-container" v-if="qid===5">
+          <input type="text" placeholder="답변:" v-model="inputText">
+          <button class="submit-btn" @click="goNext(null, qid + 1)">추천 결과 보기</button>
         </div>
       </div>
     </main>
@@ -40,6 +44,8 @@ const router = useRouter()
 
 const qid = ref(parseInt(route.params.qid))
 
+const inputText = ref("");
+
 watch(
   () => route.params.qid,
   (newQid) => {
@@ -50,7 +56,7 @@ watch(
 const questions = [
   {
     qid: 1,
-    label: '질문1',
+    label: '오늘의 날씨는 어떤가요?',
     leftImg: sunnyImg,
     leftText: '맑아요',
     rightImg: cloudyImg,
@@ -58,39 +64,42 @@ const questions = [
   },
   {
     qid: 2,
-    label: '질문2',
+    label: '오늘의 기분은 어떤가요?',
     leftImg: sunnyImg,
-    leftText: '맑아요',
+    leftText: '좋아요!',
     rightImg: cloudyImg,
-    rightText: '흐려요'
+    rightText: '나빠요!'
   },
   {
     qid: 3,
-    label: '질문3',
+    label: '몇 명이 먹는 음식인가요?',
     leftImg: sunnyImg,
-    leftText: '맑아요',
+    leftText: '혼자 먹어요',
     rightImg: cloudyImg,
-    rightText: '흐려요'
+    rightText: '여러명이 먹어요'
   },
   {
     qid: 4,
-    label: '질문4',
+    label: '채식 또는 비건 식단을 따르시나요?',
     leftImg: sunnyImg,
-    leftText: '맑아요',
+    leftText: '네',
     rightImg: cloudyImg,
-    rightText: '흐려요'
+    rightText: '아니요'
   },
   {
     qid: 5,
-    label: '질문5',
-    leftImg: sunnyImg,
-    leftText: '맑아요',
-    rightImg: cloudyImg,
-    rightText: '흐려요'
+    label: '제가 또 알아야 하는게 있을까요?',
   }
 ]
 
-const goNext = (nextQid) => {
+const goNext = (selectedText, nextQid) => {
+  let responses = JSON.parse(localStorage.getItem('question_responses')) || [];
+
+  if (nextQid <= 5) responses.push({ qid: qid.value, response: selectedText });
+  else responses.push({ qid: qid.value, response: inputText.value });
+
+  localStorage.setItem('question_responses', JSON.stringify(responses));
+
   nextQid <= 5 ? router.push(`/question/${nextQid}`) : router.push(`/question/result`)
 }
 </script>
@@ -125,10 +134,11 @@ main {
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 58.5rem;
+  width: 72rem;
   height: 8rem;
   background-color: var(--white-color);
   border-radius: 5rem;
+  color: var(--black-color)
 }
 
 .card-board-container {
@@ -136,5 +146,48 @@ main {
   flex-direction: row;
   gap: 4rem;
   margin-top: 5.4rem;
+}
+
+.input-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 6rem;
+  margin-top: 16rem;
+}
+
+.input-container input {
+  width: 59rem;
+  height: 10rem;
+  padding-left: 4rem;
+  border: none;
+  border-radius: 1.4rem;
+  font-size: 3.8rem;
+  font-weight: 700;
+  color: var(--black-color);
+  cursor: pointer;
+}
+
+.submit-btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 34rem;
+  height: 9rem;
+  background-color: var(--light-yellow-color);
+  border: none;
+  border-radius: 1.4rem;
+  box-shadow: 0.5rem 0.3rem 0.3rem 0rem rgba(60, 60, 60, 0.5);
+  font-size: 3.5rem;
+  font-weight: 700;
+  color: var(--black-color);
+  cursor: pointer;
+  transition: 0.3s ease-out;
+}
+
+.submit-btn:hover {
+  transform: translateY(-0.1rem);
+  box-shadow: 0.5rem 0.3rem 0.3rem 0.05rem rgba(60, 60, 60, 0.5);
 }
 </style>
