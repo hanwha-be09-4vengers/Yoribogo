c4b09155daab8678bd4a9ba03b5da52d50cda4ff
Kibeom Jeon <kibeom1145@gmail.com>
2024-10-13 00:34:40 +0900

    Feat: 요리 추천 결과 보여주기 구현

--- client/src/views/ResultView.vue
+++ client/src/views/ResultView.vue
@@ -5,14 +5,16 @@
     </header>
     <main>
       <div class="result-container">
-        <p>요리보고가 추천하는 음식은!</p>
-        <div class="result-info">
-          <span :style="{ opacity: isFlipped ? 1 : 0 }">{{ result.label }}</span>
+        <p v-if="!isLoading">요리보고가 추천하는 요리는!</p>
+        <p v-if="isLoading">요리보고가 추천 중입니다...</p>
+        <div class="result-info" v-if="!isLoading">
+          <span :style="{ opacity: isFlipped ? 1 : 0 }">{{ menuName }}</span>
         </div>
-        <div class="card-board-container">
-          <ResultBoard :img="result.img" :text="result.label" @flipped="isFlipped = !isFlipped">
+        <div class="card-board-container" v-if="!isLoading">
+          <ResultBoard :img="menuImage" :text="menuName" @flipped="isFlipped = !isFlipped">
           </ResultBoard>
         </div>
+        <Loading v-if="isLoading"></Loading>
       </div>
     </main>
   </div>
@@ -21,20 +23,45 @@
 <script setup>
 import ResultNav from '../components/ResultNav.vue'
 import ResultBoard from '../components/ResultBoard.vue'
+import Loading from '@/components/Loading.vue'
 import bibimbapImg from '../assets/bibimbap.svg'
-import { useRoute, useRouter } from 'vue-router'
-import { ref } from 'vue'
-
-const route = useRoute()
-const router = useRouter()
+import { ref, onMounted} from 'vue'
+import axios from 'axios'
 
 const isFlipped = ref(false)
+const isLoading = ref(true)
 
-const result = {
-  rid: 1,
-  label: '비빔밥',
-  img: bibimbapImg
+const menuName = ref('')
+const menuImage = ref(bibimbapImg)
+const recipeId = ref()
+
+const fetchRecommendedMenu = async () => {
+  const questionResponse = JSON.parse(localStorage.getItem('question_responses'));
+  const requestData = {
+    first: questionResponse[0].response, 
+    second: questionResponse[1].response,
+    third: questionResponse[2].response,
+    fourth: questionResponse[3].response,
+    fifth: questionResponse[4].response,
+  };
+  try {
+    const response = (await axios.post('/api/recipes/recommend',requestData)).data;
+    if(response.success) {
+      menuName.value = response.data.menu_name;
+      if(response.data.menu_image) menuImage.value = response.data.menu_image;
+      recipeId.value = response.data.recipe_id; 
+      isLoading.value = false;
+      localStorage.removeItem("question_responses");
+    }
+  } catch (error) {
+    console.error('요리를 추천받는데 실패했습니다.', error)
+  }
 }
+
+onMounted(() => {
+  fetchRecommendedMenu();  
+})
+
 </script>
 
 <style scoped>
@@ -73,7 +100,8 @@ main {
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 22.5rem;
+  min-width: 15rem; /* 최소 너비 설정 */
+  padding: 1rem 2rem; /* 내부 여백 추가 */
   height: 8rem;
   background-color: var(--light-yellow-color);
   border-radius: 5rem;
@@ -81,6 +109,7 @@ main {
 
 .result-info span {
   transition: opacity 0.5s ease;
+  white-space: nowrap;
 }
 
 .card-board-container {
