2c1b285ab4e389b72ede5427341f27fc9438a0d2
Haegwan-Choe <kkhagwan@naver.com>
2024-10-15 22:39:41 +0900

    Feat: 나만의 레시피 수정

--- server/src/main/java/com/avengers/yoribogo/recipeboard/repository/RecipeBoardManualRepository.java
+++ server/src/main/java/com/avengers/yoribogo/recipeboard/repository/RecipeBoardManualRepository.java
@@ -2,6 +2,10 @@ package com.avengers.yoribogo.recipeboard.repository;
 
 import com.avengers.yoribogo.recipeboard.domain.RecipeBoardManual;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface RecipeBoardManualRepository extends JpaRepository<RecipeBoardManual, Long> {
+
+    @Transactional
+    void deleteByRecipeBoardRecipeBoardId(Long recipeBoardId);
 }
