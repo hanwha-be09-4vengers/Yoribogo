c92586c21f9d5f6c03878988e8271243fe9345df
iwillbfine <iwillbfine@naver.com>
2024-10-15 09:26:45 +0900

    테스트 코드 추가

--- server/src/main/java/com/avengers/yoribogo/inquiry/domain/Inquiry.java
+++ server/src/main/java/com/avengers/yoribogo/inquiry/domain/Inquiry.java
@@ -46,4 +46,6 @@ public class Inquiry {
         this.inquiryContent = inquiryContent;
         this.userId = userId;
     }
+
+
 }
--- server/src/main/java/com/avengers/yoribogo/inquiry/repository/InquiryRepository.java
+++ server/src/main/java/com/avengers/yoribogo/inquiry/repository/InquiryRepository.java
@@ -3,5 +3,6 @@ package com.avengers.yoribogo.inquiry.repository;
 import com.avengers.yoribogo.inquiry.domain.Inquiry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface InquiryRepository extends JpaRepository<Inquiry, Integer> {
 }
