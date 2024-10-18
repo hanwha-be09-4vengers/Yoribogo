1948f848afbbf774eda01d4a160214d8fc1c6d28
changuk2 <changuk0308@naver.com>
2024-10-18 09:49:00 +0900

    Feat: JwtUtilTest 추가

--- server/src/test/java/com/avengers/yoribogo/user/service/UserServiceTest.java
+++ server/src/test/java/com/avengers/yoribogo/user/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.avengers.yoribogo.user.service;
 
+import com.amazonaws.AmazonClientException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.avengers.yoribogo.common.exception.CommonException;
@@ -60,5 +61,46 @@ class UserServiceTest {
         assertNotNull(result);
         assertEquals("새로운 닉네임", result.getNickname());
         assertEquals("https://s3.amazonaws.com/bucket/test.jpg", result.getProfileImage());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 사용자 프로필 업데이트 실패 테스트")
+    void updateProfile_UserNotFound() {
+        // Given
+        Long userId = 999L;  // 존재하지 않는 사용자 ID로 테스트
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("profileImage", "test.jpg",
+                "image/jpeg", "test image content".getBytes());
+
+        RequestUpdateUserDTO userUpdateDTO = new RequestUpdateUserDTO();
+        userUpdateDTO.setNickname("새로운 닉네임");
+        userUpdateDTO.setProfileImage(mockMultipartFile);
+
+        // When & Then
+        CommonException exception = assertThrows(CommonException.class, () -> userService.updateProfile(userId, userUpdateDTO));
+        assertEquals(ErrorCode.NOT_FOUND_USER, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("프로필 이미지 업로드 실패 테스트")
+    void updateProfile_FileUploadFailure() throws IOException {
+        // Given
+        Long userId = 1L;
+        UserEntity userEntity = userRepository.findById(userId)
+                .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_USER));
+
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("profileImage", "test.jpg",
+                "image/jpeg", "test image content".getBytes());
+
+        RequestUpdateUserDTO userUpdateDTO = new RequestUpdateUserDTO();
+        userUpdateDTO.setNickname("새로운 닉네임");
+        userUpdateDTO.setProfileImage(mockMultipartFile);
+
+        // S3 파일 업로드 실패 Mocking
+        Mockito.doThrow(new AmazonClientException("S3 upload failed"))
+                .when(s3Client).putObject(Mockito.any(PutObjectRequest.class));
+
+        // When & Then
+        CommonException exception = assertThrows(CommonException.class, () -> userService.updateProfile(userId, userUpdateDTO));
+        assertEquals(ErrorCode.FILE_UPLOAD_ERROR, exception.getErrorCode());
     }
 }
