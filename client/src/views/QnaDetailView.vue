aeca964287b641ee64e1c52f560135bb218dd132
Kibeom Jeon <kibeom1145@gmail.com>
2024-10-20 23:51:22 +0900

    Style: css 수정

--- client/src/views/QnaDetailView.vue
+++ client/src/views/QnaDetailView.vue
@@ -9,7 +9,7 @@
       <div class="wrapper">
         <div class="L1">
           <div class="title">
-            <h1 v-bind="inquiry">Q {{inquiry.inquiryTitle}}</h1>
+            <h1 v-bind="inquiry">Q. {{inquiry.inquiryTitle}}</h1>
             <img class="privateIcon" v-if="inquiry.inquiryVisibility === 'PRIVATE'" src="/src/assets/images/private_icon.png"/>
             <div class="visibility1" v-if="inquiry.answerStatus == 'PENDING'">
               답변대기
@@ -57,9 +57,9 @@
               </div>
               <div class="content1">
                 <p>{{ ans.answerContent }}</p>
-              </div>
-              <div v-if="ans.user.userId === user.user_id" v-bind="user">
-                <p class="delete-btn" @click="delAnswer(ans)">삭제</p>
+                <div v-if="ans.user.userId === user.user_id" v-bind="user">
+                  <p class="delete-btn" @click="delAnswer(ans)">x</p>
+                </div>
               </div>
             </div>
             <div class="enterprise" v-else>
@@ -72,9 +72,9 @@
               </div>
               <div class="content2">
                 <p>{{ ans.answerContent }}</p>
-              </div>
-              <div v-if="ans.user.userId === user.user_id" v-bind="user">
-                <p class="delete-btn" @click="delAnswer(ans)">삭제</p>
+                <div v-if="ans.user.userId === user.user_id" v-bind="user">
+                  <p class="delete-btn" @click="delAnswer(ans)">x</p>
+                </div>
               </div>
             </div>
           </div>
@@ -153,7 +153,7 @@ import MainBoard from '@/components/common/MainBoard.vue'
     if (inquiry.value.user.userId === user.value.user_id ||
           user.value.user_role === 'ADMIN') {
       setDTO();
-      const result = await addAnswer(toRaw(dto.value));
+      await addAnswer(toRaw(dto.value));
       router.go(0);    
     } else {
       alert('문의 작성자만 작성 가능합니다.');
@@ -162,7 +162,7 @@ import MainBoard from '@/components/common/MainBoard.vue'
   };
 
   const delAnswer = async (ans) => {
-    const result = await deleteAnswer(ans.answerId);
+    await deleteAnswer(ans.answerId);
     router.go(0);
     alert('답변이 삭제되었습니다.');
   };
@@ -180,7 +180,7 @@ import MainBoard from '@/components/common/MainBoard.vue'
   // 삭제 후 목록으로 이동
   const remove = () => {
     if (inquiry.value.user.userId === user.value.user_id) {
-      const result = deleteInquiry(inquiry.value.inquiryId);
+      deleteInquiry(inquiry.value.inquiryId);
       alert('문의가 삭제되었습니다.');
       router.push('/qna');
     } else {
@@ -219,6 +219,207 @@ import MainBoard from '@/components/common/MainBoard.vue'
   top: 7rem;
   left: 12rem
 }
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  min-height: 60rem;
+  margin-top: 8rem;
+  overflow: hidden;
+  font-size: 2.4rem;
+}
+
+.L1 {
+  display: flex;
+  height: 10%;
+  align-items: flex-end;
+}
+
+.title {
+  display: flex;
+  align-items: center;
+  width: 80%;
+  margin-bottom: 2rem;
+  gap: 2rem;
+}
+
+h1 {
+  font-size: 30px;
+}
+
+.privateIcon {
+  height: 2rem;
+  align-self: center;
+}
+
+.visibility1, .visibility2 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 3.6rem;
+  width: 8rem;
+}
+
+.visibility1 {
+  font-size: 1.2rem;
+  border-radius: 2rem;
+  background: #aaa;
+  color: var(--white-color);
+}
+
+.visibility2 {
+  font-size: 1.2rem;
+  border-radius: 2rem;
+  background: var(--pink-color);
+  color: var(--white-color);
+}
+
+.icons {
+  width: 20%;
+  position: relative;
+  display: inline-block;
+  text-align: right;
+  padding-bottom: 1%;
+}
+
+.kebab {
+  width: 2rem; /* 아이콘 크기 조절 */
+  height: 2rem;
+  background-color: white;
+  background-image: url('@/assets/images/kebab_menu_icon.png');
+  border: none;
+  outline: none;
+  background-position: right;
+  background-size: contain; /* 이미지를 버튼 크기에 맞춤 */
+  background-repeat: no-repeat;
+  background-position: center;
+  cursor: pointer; /* 마우스 오버 시 클릭 가능한 상태로 */
+}
+
+.menus {
+  display: none; /* 기본적으로 드롭다운 메뉴는 숨김 */
+  position: absolute;
+  top: 3.8rem; /* 아이콘 바로 아래에 드롭다운 메뉴가 나타나도록 설정 */
+  right: 0;
+  background-color: var(--white-color);
+  box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.2); /* 그림자 효과 */
+  z-index: 1;
+}
+
+.menus a {
+  color: var(--black-color);
+  padding: 1.5rem 2rem;
+  text-decoration: none;
+  display: block;
+  font-size: 2rem;
+  cursor: pointer;
+}
+
+.menus a:hover {
+  background-color: #f1f1f1; /* 마우스 오버 시 배경색 변경 */
+}
+
+.active .menus {
+  display: block; /* 메뉴가 활성화되면 보이게 설정 */
+}
+
+.L2 {
+  display: flex;
+  flex-direction: column;
+  border-top: 0.1rem solid #bfc1c466;
+}
+
+.inquiry-info {
+  padding: 2rem 0 3% 1.2rem;
+}
+.inquiry_content {
+  padding: 0 0 5% 1.2rem;
+  border-bottom: 0.1rem solid #bfc1c466;
+}
+.write {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 3rem 0 3rem 0;
+  gap: 2rem;
+}
+
+.group {
+  width: 100%;
+  display: grid;
+  height: 4rem;
+  grid-template-columns: 90% 10%;
+  border: 0.5px solid #bfc1c466;
+  justify-items: end;
+}
+
+.group input {
+  width: 100%;
+  border: none;
+  outline: none;
+  padding-left: 1.2rem;
+}
+
+.group button {
+  justify-items: right;
+  border: none;
+  color: white;
+  width: 7rem;
+  background-color: var(--navy-color);
+  cursor: pointer;
+}
+
+.answers {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin-bottom: 0.2rem;
+}
+
+.admin, .enterprise {
+  display: grid;
+  height: 9%;
+  grid-template-rows: 1fr 2fr 0.5fr;
+  padding: 1.2rem;
+}
+
+.admin {
+  background-color: #FFF5F5;
+}
+.ans, .inquiry-info {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  font-size: 1.5rem;
+}
+.info1, .info2 {
+  display: flex;
+  column-gap: 1.2rem;
+}
+.info1 p:first-child,
+.info2 p:first-child {
+  font-weight: bold;
+}
+.content1, .content2 {
+  display: flex;
+  justify-content: space-between;
+  padding-left: 5rem;
+  margin-top: 0.8rem;
+}
+
+.icon {
+  width: 4rem;
+  height: 4rem;
+  margin: 0 1.2rem 0 0;
+}
+.delete-btn {
+  text-align: right;
+  margin: 0 1.2rem 0.8rem 0;
+  color: rgb(75, 75, 75);
+  cursor: pointer;
+}
+
 @media screen and (max-width: 480px) {
   .notification-btn {
     left: 10rem;
@@ -259,184 +460,5 @@ import MainBoard from '@/components/common/MainBoard.vue'
   .home-btn {
     right: 16rem;
   }
-}
-.wrapper {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-  padding: 60px 60px 20px 60px;
-  overflow: hidden;
-  font-size: 2.4rem;
-}
-.L1 {
-  display: flex;
-  height: 10%;
-  align-items: flex-end;
-}
-.title {
-  display: flex;
-  width: 80%;
-  column-gap: 10px;
-  align-items: flex-end;
-  padding: 0 2% 2% 2%;
-}
-h1 {
-  font-size: 30px;
-}
-.privateIcon {
-  height: 15px;
-  align-self: center;
-}
-.visibility1, .visibility2 {
-  height: 20px;
-  width: 55px;
-  margin-left: 20px;
-  align-self: flex-end;
-}
-.visibility1 {
-  text-align: center;
-  font-size: 6px;
-  border-radius: 16px;
-  background: #EFEFEF;
-  color: #B9B9B9;
-}
-.visibility2 {
-  text-align: center;
-  font-size: 6px;
-  border-radius: 16px;
-  background: #333;
-  color: white;
-}
-.icons {
-  width: 20%;
-  position: relative;
-  display: inline-block;
-  text-align: right;
-  padding-bottom: 1%;
-}
-
-.kebab {
-  width: 15px; /* 아이콘 크기 조절 */
-  height: 15px;
-  background-color: white;
-  background-image: url('@/assets/images/kebab_menu_icon.png');
-  border: none;
-  outline: none;
-  background-position: right;
-  background-size: contain; /* 이미지를 버튼 크기에 맞춤 */
-  background-repeat: no-repeat;
-  background-position: center;
-  cursor: pointer; /* 마우스 오버 시 클릭 가능한 상태로 */
-}
-
-.menus {
-  display: none; /* 기본적으로 드롭다운 메뉴는 숨김 */
-  position: absolute;
-  top: 30px; /* 아이콘 바로 아래에 드롭다운 메뉴가 나타나도록 설정 */
-  right: 0;
-  background-color: white;
-  box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.2); /* 그림자 효과 */
-  z-index: 1;
-}
-
-.menus a {
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-  font-size: 14px;
-  cursor: pointer;
-}
-.menus a:hover {
-  background-color: #f1f1f1; /* 마우스 오버 시 배경색 변경 */
-}
-.active .menus {
-  display: block; /* 메뉴가 활성화되면 보이게 설정 */
-}
-.L2 {
-  display: flex;
-  flex-direction: column;
-  border-top: 0.5px solid #bfc1c466;
-}
-.inquiry-info {
-  padding: 10px 0 3% 10px;
-}
-.inquiry_content {
-  padding: 0 0 5% 10px;
-  border-bottom: 0.5px solid #bfc1c466;
-}
-.write {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  padding: 20px 0 20px 0;
-  row-gap: 10px;
-}
-.group {
-  width: 100%;
-  display: grid;
-  height: 30px;
-  grid-template-columns: 90% 10%;
-  border: 0.5px solid #bfc1c466;
-  justify-items: end;
-}
-.group input {
-  width: 100%;
-  border: none;
-  outline: none;
-  padding-left: 10px;
-}
-.group button {
-  justify-items: right;
-  border: none;
-  color: white;
-  width: 50px;
-  background-color: black;
-  cursor: pointer;
-}
-.answers {
-  display: flex;
-  flex-direction: column;
-  row-gap: 5px;
-  margin-bottom: 1px;
-}
-.admin, .enterprise {
-  display: grid;
-  height: 9%;
-  grid-template-rows: 1fr 2fr 0.5fr;
-  padding: 10px;
-}
-.admin {
-  background-color: #FFF5F5;
-}
-.ans, .inquiry-info {
-  display: flex;
-  width: 100%;
-  align-items: center;
-  font-size: 1.5rem;
-}
-.info1, .info2 {
-  display: flex;
-  column-gap: 10px;
-}
-.info1 p:first-child,
-.info2 p:first-child {
-  font-weight: bold;
-}
-.content1, .content2 {
-  padding-left: 40px;
-  margin-top: 5px;
-}
-.icon {
-  width: 30px;
-  height: 30px;
-  margin: 0 10px 0 0;
-}
-.delete-btn {
-  text-align: right;
-  margin: 0 10px 5px 0;
-  color: rgb(75, 75, 75);
-  cursor: pointer;
 }
 </style>
