cb0a9075dd75d4c92d34d9100411c2a0366a0520
woodart8 <kibeom1145@gmail.com>
2024-10-22 21:29:16 +0900

    Perf: 요리 추천 매뉴얼 생성 WebFlux 처리

--- server/src/main/java/com/avengers/yoribogo/openai/dto/ResponseChatDTO.java
+++ server/src/main/java/com/avengers/yoribogo/openai/dto/ResponseChatDTO.java
@@ -5,11 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ResponseChatDTO {
+public class ResponseChatDTO implements Serializable {
     private List<Choice> choices;
 }
--- server/src/main/java/com/avengers/yoribogo/recipe/service/RecipeManualService.java
+++ server/src/main/java/com/avengers/yoribogo/recipe/service/RecipeManualService.java
@@ -1,7 +1,10 @@
 package com.avengers.yoribogo.recipe.service;
 
 import com.avengers.yoribogo.recipe.dto.RecipeManualDTO;
+import com.avengers.yoribogo.recipe.dto.RequestAIRecipeManualDTO;
 import com.avengers.yoribogo.recipe.dto.RequestRecipeManualDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -17,6 +20,6 @@ public interface RecipeManualService {
     List<RecipeManualDTO> modifyRecipeManual(Long recipeId, RequestRecipeManualDTO requestRecipeManualDTO);
 
     // AI 생성 매뉴얼 등록
-    void registAIRecipeManual(Long recipeId, String recipePrompt);
+    Flux<String> registAIRecipeManual(Long recipeId, RequestAIRecipeManualDTO requestAIRecipeManualDTO);
 
 }
